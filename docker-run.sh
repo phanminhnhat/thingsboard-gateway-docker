384cd6b9636aaf8598bd8319e9772d902c729d54
NhatPhan <minhnhat247@gmail.com>
2019-06-05 16:13:37 +0700

    Update docker-run.sh

--- docker-run.sh
+++ docker-run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo docker run -d -v ~/.mytb-gateway-data:/etc/tb-gateway/conf -v ~/.mytb-gateway-logs:/var/log/tb-gateway/ --name tb-gateway-instance tb-gate-way
+sudo docker run -d -v ~/.mytb-gateway-data:/etc/tb-gateway/conf -v ~/.mytb-gateway-logs:/var/log/tb-gateway/ --name tb-gateway-instance tb-gate-way --entrypoint "/run-application.sh"
 
